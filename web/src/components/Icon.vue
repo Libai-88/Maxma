7ba8894247fa8a61ed6e8a16a1ffe99976cd2508
Your Name <your.email@example.com>
2026-07-17 19:15:58 +0800

    feat(icons): register 35 new SVG icons in Icon.vue

--- web/src/components/Icon.vue
+++ web/src/components/Icon.vue
@@ -27,6 +27,45 @@ import sparklesRaw from '@/assets/icons/chat-input/sparkles.svg?raw'
 import toolRaw from '@/assets/icons/chat-input/tool.svg?raw'
 import sendRaw from '@/assets/icons/chat-input/send.svg?raw'
 import stopRaw from '@/assets/icons/chat-input/stop.svg?raw'
+// sidebar/
+import playgroundRaw from '@/assets/icons/sidebar/playground.svg?raw'
+// chat-input/
+import citeRaw from '@/assets/icons/chat-input/cite.svg?raw'
+import closeRaw from '@/assets/icons/chat-input/close.svg?raw'
+// tools/
+import arrowRightRaw from '@/assets/icons/tools/arrow-right.svg?raw'
+import bikeRaw from '@/assets/icons/tools/bike.svg?raw'
+import busRaw from '@/assets/icons/tools/bus.svg?raw'
+import calendarRaw from '@/assets/icons/tools/calendar.svg?raw'
+import checkmarkRaw from '@/assets/icons/tools/checkmark.svg?raw'
+import chevronDownRaw from '@/assets/icons/tools/chevron-down.svg?raw'
+import chevronRightRaw from '@/assets/icons/tools/chevron-right.svg?raw'
+import circleOutlineRaw from '@/assets/icons/tools/circle-outline.svg?raw'
+import codeQualityRaw from '@/assets/icons/tools/code-quality.svg?raw'
+import cookieRaw from '@/assets/icons/tools/cookie.svg?raw'
+import docReaderRaw from '@/assets/icons/tools/doc-reader.svg?raw'
+import errorRaw from '@/assets/icons/tools/error.svg?raw'
+import filePageRaw from '@/assets/icons/tools/file-page.svg?raw'
+import folderRaw from '@/assets/icons/tools/folder.svg?raw'
+import mapPinRaw from '@/assets/icons/tools/map-pin.svg?raw'
+import pdfReaderRaw from '@/assets/icons/tools/pdf-reader.svg?raw'
+import pythonRaw from '@/assets/icons/tools/python.svg?raw'
+import scraperRaw from '@/assets/icons/tools/scraper.svg?raw'
+import searchRaw from '@/assets/icons/tools/search.svg?raw'
+import syntaxErrorRaw from '@/assets/icons/tools/syntax-error.svg?raw'
+import syntaxOkRaw from '@/assets/icons/tools/syntax-ok.svg?raw'
+import todoDueRaw from '@/assets/icons/tools/todo-due.svg?raw'
+import todoProjectRaw from '@/assets/icons/tools/todo-project.svg?raw'
+import walkRaw from '@/assets/icons/tools/walk.svg?raw'
+import warningRaw from '@/assets/icons/tools/warning.svg?raw'
+// weather/
+import weatherThunderRaw from '@/assets/icons/weather/thunder.svg?raw'
+import weatherSnowRaw from '@/assets/icons/weather/snow.svg?raw'
+import weatherRainRaw from '@/assets/icons/weather/rain.svg?raw'
+import weatherFogRaw from '@/assets/icons/weather/fog.svg?raw'
+import weatherSunnyRaw from '@/assets/icons/weather/sunny.svg?raw'
+import weatherCloudyRaw from '@/assets/icons/weather/cloudy.svg?raw'
+import weatherPartlyCloudyRaw from '@/assets/icons/weather/partly-cloudy.svg?raw'
 
 const props = withDefaults(defineProps<{
   name: string
@@ -63,6 +102,41 @@ const svgContents: Record<string, string> = {
   tool: toolRaw,
   send: sendRaw,
   stop: stopRaw,
+  playground: playgroundRaw,
+  cite: citeRaw,
+  close: closeRaw,
+  'arrow-right': arrowRightRaw,
+  bike: bikeRaw,
+  bus: busRaw,
+  calendar: calendarRaw,
+  checkmark: checkmarkRaw,
+  'chevron-down': chevronDownRaw,
+  'chevron-right': chevronRightRaw,
+  'circle-outline': circleOutlineRaw,
+  'code-quality': codeQualityRaw,
+  cookie: cookieRaw,
+  'doc-reader': docReaderRaw,
+  error: errorRaw,
+  'file-page': filePageRaw,
+  folder: folderRaw,
+  'map-pin': mapPinRaw,
+  'pdf-reader': pdfReaderRaw,
+  python: pythonRaw,
+  scraper: scraperRaw,
+  search: searchRaw,
+  'syntax-error': syntaxErrorRaw,
+  'syntax-ok': syntaxOkRaw,
+  'todo-due': todoDueRaw,
+  'todo-project': todoProjectRaw,
+  walk: walkRaw,
+  warning: warningRaw,
+  'weather-thunder': weatherThunderRaw,
+  'weather-snow': weatherSnowRaw,
+  'weather-rain': weatherRainRaw,
+  'weather-fog': weatherFogRaw,
+  'weather-sunny': weatherSunnyRaw,
+  'weather-cloudy': weatherCloudyRaw,
+  'weather-partly-cloudy': weatherPartlyCloudyRaw,
   sticker: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
   <path d="M15.5 3H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V8.5L15.5 3Z"/>
   <path d="M14 2v6h6"/>
